46845c405e06df699c734a4b63ce5e1cd57516e9
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2025-12-09 12:08:00 +0530

    replicated image pages for tv shows

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ load_dotenv()
 TMDB_KEY = os.getenv("TMDB_API_KEY")
 
 film_endpoint = "https://api.themoviedb.org/3/movie"
+tv_endpoint = "https://api.themoviedb.org/3/tv"
 headers = {"Authorization": TMDB_KEY, "accept": "application/json"}
 search_params = {"query": None}
 img_params = {"include_image_language": "en-US"}
@@ -19,6 +20,14 @@ def fetch_film(id):
 
 def fetch_images(id):
     response = requests.get(f"https://api.themoviedb.org/3/movie/{id}/images", headers=headers, params=img_params, timeout=10)
+    return response.json()
+
+def fetch_tv(id):
+    response = requests.get(f"{tv_endpoint}/{id}", headers=headers, timeout=10)
+    return response.json()
+
+def fetch_tv_images(id):
+    response = requests.get(f"https://api.themoviedb.org/3/tv/{id}/images", headers=headers, params=img_params, timeout=10)
     return response.json()
 
 @app.route("/")
@@ -74,7 +83,36 @@ def results_tv():
 
     response = requests.get(tv_search_endpont, headers=headers, params=search_params)
     results = response.json()['results']
-    return render_template("results_tv.html", search_query=search_query, results=results)
+    return render_template("tv/results_tv.html", search_query=search_query, results=results)
+
+@app.route("/tv/<id>")
+def tv_page(id):
+    data = fetch_tv(id)
+    images = fetch_tv_images(id)
+    return render_template("tv/tv_page.html", data=data, posters=images['posters'])
+
+
+@app.route("/tv/<id>/posters")
+def tv_posters(id):
+    data = fetch_tv(id)
+    images = fetch_tv_images(id)
+
+    return render_template("tv/tv_posters.html", data=data, posters=images['posters'])
+
+@app.route("/tv/<id>/backdrops")
+def tv_backdrops(id):
+    data = fetch_tv(id)
+    images = fetch_tv_images(id)
+
+    return render_template("tv/tv_backdrops.html", data=data, backdrops=images['backdrops'])
+
+
+@app.route("/tv/<id>/logos")
+def tv_logos(id):
+    data = fetch_tv(id)
+    images = fetch_tv_images(id)
+
+    return render_template("tv/tv_logos.html", data=data, logos=images['logos'])
 
 
 if __name__ == "__main__":
